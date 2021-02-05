d1523d3f416a97226bac58a97058f3fffe618def
Stephen Taylor <steve.taylor1987@gmail.com>
2021-02-05 13:42:36 -0600

    setting usetex=False in all code

--- lectures/code/fig_uniform_distribution.py
+++ lectures/code/fig_uniform_distribution.py
@@ -41,7 +41,7 @@ from matplotlib import pyplot as plt
 # result in an error if LaTeX is not installed on your system.  In that case,
 # you can set usetex to False.
 from astroML.plotting import setup_text_plots
-setup_text_plots(fontsize=8, usetex=True)
+setup_text_plots(fontsize=8, usetex=False)
 
 #------------------------------------------------------------
 # Define the distribution parameters to be plotted
